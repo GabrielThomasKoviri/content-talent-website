4f3414332fc51e5489fcf2c30101d9fa82487ebb
GabrielThomasKoviri <gabrielthomas6699@gmail.com>
2026-08-01 21:02:03 +0530

    chore: remove backend files and update .gitignore

--- app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.base import BaseModel
-from app.models.user import User
-from app.models.video import Video
-from app.models.playlist import Playlist, PlaylistVideo
-
-__all__ = ["BaseModel", "User", "Video", "Playlist", "PlaylistVideo"]
